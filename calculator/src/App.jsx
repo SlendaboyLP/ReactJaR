c3712bd5fa30b8f1997cc28d14e74434e8985daf
Alexander Zeitlhofer <alexander.zeitlhofer@sz-ybbs.ac.at>
2024-04-24 18:54:55 +0200

    added new buttons

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -15,35 +15,47 @@ export default function App() {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="bg-gray-300 rounded-md w-min h-min p-4">
+      <div className="bg-gray-300 rounded-md  p-4">
         <textarea
           value={result}
-          className="w-48  mb-4 resize-none rounded-md"
+          className=" w-full mb-6 resize-none rounded-md h-16"
         ></textarea>
         <div>
           <CalcButton value="1" onClick={() => handleClick("1")} />
           <CalcButton value="2" onClick={() => handleClick("2")} />
           <CalcButton value="3" onClick={() => handleClick("3")} />
           <CalcButton value="+" onClick={() => handleClick("+")} />
+
+          <CalcButton
+            value="<-"
+            onClick={() => setResult(result.slice(0, result.length - 1))}
+          ></CalcButton>
         </div>
         <div>
           <CalcButton value="4" onClick={() => handleClick("4")} />
           <CalcButton value="5" onClick={() => handleClick("5")} />
           <CalcButton value="6" onClick={() => handleClick("6")} />
           <CalcButton value="-" onClick={() => handleClick("-")} />
+          <CalcButton value="(" onClick={() => handleClick("(")}></CalcButton>
         </div>
         <div>
           <CalcButton value="7" onClick={() => handleClick("7")} />
           <CalcButton value="8" onClick={() => handleClick("8")} />
           <CalcButton value="9" onClick={() => handleClick("9")} />
           <CalcButton value="*" onClick={() => handleClick("*")} />
+          <CalcButton value=")" onClick={() => handleClick(")")}></CalcButton>
         </div>
         <div>
           <CalcButton value="C" onClick={() => setResult("")} />
           <CalcButton value="0" onClick={() => handleClick("0")} />
           <CalcButton value="." onClick={() => handleClick(".")} />
+          <CalcButton
+            value="√"
+            onClick={() => setResult(Math.sqrt(result))}
+          ></CalcButton>
           <CalcButton value="=" onClick={() => calculateResult()} />
         </div>
+        <div></div>
       </div>
     </div>
   );
